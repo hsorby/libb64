635cdf009142722b14c2154039731c2fa9bd96df
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-12-12 12:37:32 +1300

    Rename src/base64.cc to src/base64.cxx.

--- src/base64.cxx
+++ src/base64.cxx
@@ -1,5 +1,5 @@
 /*
-base64.cc - c++ source to a base64 reference encoder and decoder
+base64.cxx - c++ source to a base64 reference encoder and decoder
 
 This is part of the libb64 project, and has been placed in the public domain.
 For details, see http://sourceforge.net/projects/libb64

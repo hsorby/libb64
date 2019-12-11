2c3f3654e50ac5c8bad8c122f1029284da9e4ccf
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-12-12 12:41:57 +1300

    Clean whitespace on all files.

--- examples/c-example1.c
+++ examples/c-example1.c
@@ -29,80 +29,80 @@ For a more realistic example, see c-example2.c
 
 char* encode(const char* input)
 {
-	/* set up a destination buffer large enough to hold the encoded data */
-	char* output = (char*)malloc(SIZE);
-	/* keep track of our encoded position */
-	char* c = output;
-	/* store the number of bytes encoded by a single call */
-	int cnt = 0;
-	/* we need an encoder state */
-	base64_encodestate s;
-	
-	/*---------- START ENCODING ----------*/
-	/* initialise the encoder state */
-	base64_init_encodestate(&s);
-	/* gather data from the input and send it to the output */
-	cnt = base64_encode_block(input, strlen(input), c, &s);
-	c += cnt;
-	/* since we have encoded the entire input string, we know that 
-	   there is no more input data; finalise the encoding */
-	cnt = base64_encode_blockend(c, &s);
-	c += cnt;
-	/*---------- STOP ENCODING  ----------*/
-	
-	/* we want to print the encoded data, so null-terminate it: */
-	*c = 0;
-	
-	return output;
+    /* set up a destination buffer large enough to hold the encoded data */
+    char* output = (char*)malloc(SIZE);
+    /* keep track of our encoded position */
+    char* c = output;
+    /* store the number of bytes encoded by a single call */
+    int cnt = 0;
+    /* we need an encoder state */
+    base64_encodestate s;
+
+    /*---------- START ENCODING ----------*/
+    /* initialise the encoder state */
+    base64_init_encodestate(&s);
+    /* gather data from the input and send it to the output */
+    cnt = base64_encode_block(input, strlen(input), c, &s);
+    c += cnt;
+    /* since we have encoded the entire input string, we know that
+       there is no more input data; finalise the encoding */
+    cnt = base64_encode_blockend(c, &s);
+    c += cnt;
+    /*---------- STOP ENCODING  ----------*/
+
+    /* we want to print the encoded data, so null-terminate it: */
+    *c = 0;
+
+    return output;
 }
 
 char* decode(const char* input)
 {
-	/* set up a destination buffer large enough to hold the encoded data */
-	char* output = (char*)malloc(SIZE);
-	/* keep track of our decoded position */
-	char* c = output;
-	/* store the number of bytes decoded by a single call */
-	int cnt = 0;
-	/* we need a decoder state */
-	base64_decodestate s;
-	
-	/*---------- START DECODING ----------*/
-	/* initialise the decoder state */
-	base64_init_decodestate(&s);
-	/* decode the input data */
-	cnt = base64_decode_block(input, strlen(input), c, &s);
-	c += cnt;
-	/* note: there is no base64_decode_blockend! */
-	/*---------- STOP DECODING  ----------*/
-	
-	/* we want to print the decoded data, so null-terminate it: */
-	*c = 0;
-	
-	return output;
+    /* set up a destination buffer large enough to hold the encoded data */
+    char* output = (char*)malloc(SIZE);
+    /* keep track of our decoded position */
+    char* c = output;
+    /* store the number of bytes decoded by a single call */
+    int cnt = 0;
+    /* we need a decoder state */
+    base64_decodestate s;
+
+    /*---------- START DECODING ----------*/
+    /* initialise the decoder state */
+    base64_init_decodestate(&s);
+    /* decode the input data */
+    cnt = base64_decode_block(input, strlen(input), c, &s);
+    c += cnt;
+    /* note: there is no base64_decode_blockend! */
+    /*---------- STOP DECODING  ----------*/
+
+    /* we want to print the decoded data, so null-terminate it: */
+    *c = 0;
+
+    return output;
 }
 
 
 int main()
 {
-	const char* input = "hello world";
-	char* encoded;
-	char* decoded;
-	
-	/* encode the data */
-	encoded = encode(input);
-	printf("encoded: %s", encoded); /* encoded data has a trailing newline */
+    const char* input = "hello base64";
+    char* encoded;
+    char* decoded;
 
-	/* decode the data */
-	decoded = decode(encoded);
-	printf("decoded: %s\n", decoded);
-	
-	/* compare the original and decoded data */
-	assert(strcmp(input, decoded) == 0);
-	
-	free(encoded);
-	free(decoded);
-	return 0;
+    /* encode the data */
+    encoded = encode(input);
+    printf("encoded: %s", encoded); /* encoded data has a trailing newline */
+
+    /* decode the data */
+    decoded = decode(encoded);
+    printf("decoded: %s\n", decoded);
+
+    /* compare the original and decoded data */
+    assert(strcmp(input, decoded) == 0);
+
+    free(encoded);
+    free(decoded);
+    return 0;
 }
 
 
--- examples/c-example2.c
+++ examples/c-example2.c
@@ -26,103 +26,103 @@ encoding and decoding the data.
 
 void encode(FILE* inputFile, FILE* outputFile)
 {
-	/* set up a destination buffer large enough to hold the encoded data */
-	int size = SIZE;
-	char* input = (char*)malloc(size);
-	char* encoded = (char*)malloc(2*size); /* ~4/3 x input */
-	/* we need an encoder and decoder state */
-	base64_encodestate es;
-	/* store the number of bytes encoded by a single call */
-	int cnt = 0;
-	
-	/*---------- START ENCODING ----------*/
-	/* initialise the encoder state */
-	base64_init_encodestate(&es);
-	/* gather data from the input and send it to the output */
-	while (1)
-	{
-		cnt = fread(input, sizeof(char), size, inputFile);
-		if (cnt == 0) break;
-		cnt = base64_encode_block(input, cnt, encoded, &es);
-		/* output the encoded bytes to the output file */
-		fwrite(encoded, sizeof(char), cnt, outputFile);
-	}
-	/* since we have reached the end of the input file, we know that 
-	   there is no more input data; finalise the encoding */
-	cnt = base64_encode_blockend(encoded, &es);
-	/* write the last bytes to the output file */
-	fwrite(encoded, sizeof(char), cnt, outputFile);
-	/*---------- STOP ENCODING  ----------*/
-	
-	free(encoded);
-	free(input);
+    /* set up a destination buffer large enough to hold the encoded data */
+    int size = SIZE;
+    char* input = (char*)malloc(size);
+    char* encoded = (char*)malloc(2*size); /* ~4/3 x input */
+    /* we need an encoder and decoder state */
+    base64_encodestate es;
+    /* store the number of bytes encoded by a single call */
+    int cnt = 0;
+
+    /*---------- START ENCODING ----------*/
+    /* initialise the encoder state */
+    base64_init_encodestate(&es);
+    /* gather data from the input and send it to the output */
+    while (1)
+    {
+        cnt = fread(input, sizeof(char), size, inputFile);
+        if (cnt == 0) break;
+        cnt = base64_encode_block(input, cnt, encoded, &es);
+        /* output the encoded bytes to the output file */
+        fwrite(encoded, sizeof(char), cnt, outputFile);
+    }
+    /* since we have reached the end of the input file, we know that
+       there is no more input data; finalise the encoding */
+    cnt = base64_encode_blockend(encoded, &es);
+    /* write the last bytes to the output file */
+    fwrite(encoded, sizeof(char), cnt, outputFile);
+    /*---------- STOP ENCODING  ----------*/
+
+    free(encoded);
+    free(input);
 }
 
 void decode(FILE* inputFile, FILE* outputFile)
 {
-	/* set up a destination buffer large enough to hold the decoded data */
-	int size = SIZE;
-	char* encoded = (char*)malloc(size);
-	char* decoded = (char*)malloc(1*size); /* ~3/4 x encoded */
-	/* we need an encoder and decoder state */
-	base64_decodestate ds;
-	/* store the number of bytes encoded by a single call */
-	int cnt = 0;
-	
-	/*---------- START DECODING ----------*/
-	/* initialise the encoder state */
-	base64_init_decodestate(&ds);
-	/* gather data from the input and send it to the output */
-	while (1)
-	{
-		cnt = fread(encoded, sizeof(char), size, inputFile);
-		if (cnt == 0) break;
-		cnt = base64_decode_block(encoded, cnt, decoded, &ds);
-		/* output the encoded bytes to the output file */
-		fwrite(decoded, sizeof(char), cnt, outputFile);
-	}
-	/*---------- START DECODING  ----------*/
-	
-	free(encoded);
-	free(decoded);
+    /* set up a destination buffer large enough to hold the decoded data */
+    int size = SIZE;
+    char* encoded = (char*)malloc(size);
+    char* decoded = (char*)malloc(1*size); /* ~3/4 x encoded */
+    /* we need an encoder and decoder state */
+    base64_decodestate ds;
+    /* store the number of bytes encoded by a single call */
+    int cnt = 0;
+
+    /*---------- START DECODING ----------*/
+    /* initialise the encoder state */
+    base64_init_decodestate(&ds);
+    /* gather data from the input and send it to the output */
+    while (1)
+    {
+        cnt = fread(encoded, sizeof(char), size, inputFile);
+        if (cnt == 0) break;
+        cnt = base64_decode_block(encoded, cnt, decoded, &ds);
+        /* output the encoded bytes to the output file */
+        fwrite(decoded, sizeof(char), cnt, outputFile);
+    }
+    /*---------- START DECODING  ----------*/
+
+    free(encoded);
+    free(decoded);
 }
 
 int main(int argc, char** argv)
 {
-	FILE* inputFile;
-	FILE* encodedFile;
-	FILE* decodedFile;
-	
-	if (argc < 4)
-	{
-		printf("please supply three filenames: input, encoded & decoded\n");
-		exit(-1);
-	}
-	
-	/* encode the input file */
-	
-	inputFile   = fopen(argv[1], "r");
-	encodedFile = fopen(argv[2], "w");
-	
-	encode(inputFile, encodedFile);
-	
-	fclose(inputFile);
-	fclose(encodedFile);
+    FILE* inputFile;
+    FILE* encodedFile;
+    FILE* decodedFile;
 
-	/* decode the encoded file */
-	
-	encodedFile = fopen(argv[2], "r");
-	decodedFile = fopen(argv[3], "w");
-	
-	decode(encodedFile, decodedFile);
-	
-	fclose(encodedFile);
-	fclose(decodedFile);
+    if (argc < 4)
+    {
+        printf("please supply three filenames: input, encoded & decoded\n");
+        exit(-1);
+    }
 
-	/* we leave the original vs. decoded data comparison to 
-		diff in the Makefile */
-		
-	return 0;
+    /* encode the input file */
+
+    inputFile   = fopen(argv[1], "r");
+    encodedFile = fopen(argv[2], "w");
+
+    encode(inputFile, encodedFile);
+
+    fclose(inputFile);
+    fclose(encodedFile);
+
+    /* decode the encoded file */
+
+    encodedFile = fopen(argv[2], "r");
+    decodedFile = fopen(argv[3], "w");
+
+    decode(encodedFile, decodedFile);
+
+    fclose(encodedFile);
+    fclose(decodedFile);
+
+    /* we leave the original vs. decoded data comparison to
+        diff in the Makefile */
+
+    return 0;
 }
 
 
